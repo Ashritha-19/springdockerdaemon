5388c8c32c35c952e3d690ac8ee00fd2d321b1d9
Ashritha-19 <ashri.vig2337@gmail.com>
2024-10-18 15:13:46 +0530

    spring docker

---
+++ src/main/java/com/neoteric/springdocker/controller/DockerDemo.java
@@ -0,0 +1,18 @@
+package com.neoteric.springdocker.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerDemo {
+
+        @GetMapping(value = "/docker")
+    public String hello(){
+
+        System.out.println("ADS");
+
+        return "krish";
+
+    }
+}
